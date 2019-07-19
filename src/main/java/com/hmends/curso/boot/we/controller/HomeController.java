45b18df7a770e941fc301c3ab0a782da08cf35ff
heresson <heresson@gmail.com>
2019-07-19 18:28:04 -0300

    inclusao do home controller

---
+++ src/main/java/com/hmends/curso/boot/we/controller/HomeController.java
@@ -0,0 +1,12 @@
+package com.hmends.curso.boot.we.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+
+public class HomeController {
+	
+	@GetMapping("/")
+	public String home() {
+		return "/home";
+	}
+
+}
