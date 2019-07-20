fd3c7dec96c1e09747f8636def58f92a6fa20569
heresson <heresson@gmail.com>
2019-07-20 02:32:49 -0300

    envio dos arquivos consertados

---
+++ src/main/java/com/hmends/demoMVC/DemoMvc2Application.java
@@ -0,0 +1,13 @@
+package com.hmends.demoMVC;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoMvc2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoMvc2Application.class, args);
+	}
+
+}
